453ff326a69457f1747d25a5a705bdcb5976b3f2
gilneyjr <gilneyjnr@gmail.com>
2018-09-13 19:13:20 -0300

    Fazendo capturas e montando a AST

--- parser.lua
+++ parser.lua
@@ -3,12 +3,12 @@ local re = require 'relabel'
 -- Tabela contendo informações sobre cada tipo de erro
 local errinfo = {
 	TermErr		= 'Termo esperado após operador!',
-	FactorErr	= 'Fator esperado após operador!',
-	ExpErr		= 'Expressão esperada após abertura de parênteses!',
+	ExpErr		= 'Expressão esperada!',
 	ClBrackErr	= 'Fechamento de parênteses esperado após expressão!',
 	NumErr		= 'Parte fracionária de número é esperada após ponto flutuante!',
 	AttErr		= 'Falta expressão do lado direito da atribuição',
-  	fail		= 'Não definido!'
+	EndInput	= 'Fim da entrada esperado',
+	fail		= 'Não definido!'
 }
 
 -- Tabela contendo todos os erros correspondentes à uma entrada
@@ -31,33 +31,64 @@ local def = { save_err = save_err }
 
 -- Gramática para reconhecer expressões
 local g = re.compile([[
-	lang	<- Sp ((att / exp) Sp)* !.
-	att		<- id "=" Sp exp^AttErr
-	exp 	<- term (op1 term^TermErr)* 
-	term	<- factor (op2 factor^FactorErr)*
-	factor	<- id / num / '(' exp^ExpErr ')'^ClBrackErr
-	op1		<- ('+' / '-') Sp
-	op2		<- ('*' / '/') Sp
-	id 		<- [a-zA-Z_][a-zA-Z0-9_]* Sp
-	num		<- [0-9][0-9]*('.'([0-9]^+1)^NumErr )^-1 Sp
-	Sp      <- (%s / %nl)*
+	lang	<- Sp {| {:tag: '' -> 'lang':} {:value: {| ((att / exp) Sp)* |} :} |} (!.)^EndInput
+	att		<- {| {:tag: '' -> 'att' :} {:value: {| id eq exp^AttErr |} :} |}
+	exp 	<- {| {:tag: '' -> 'exp' :} {:value: {| term (op1 exp^ExpErr)^-1|} :} |}
+	term	<- {| {:tag: '' -> 'term' :} {:value: {| factor (op2 term^TermErr)^-1 |} :} |}
+	factor	<- {| {:tag: '' -> 'factor':} {:value: {| (id / num / opBrack exp^ExpErr clBrack^ClBrackErr) |} :} |}
+	op1		<- {| {:tag: '' -> 'op1':} {:value: ('+' / '-') :} |} Sp
+	op2		<- {| {:tag: '' -> 'op2':} {:value: ('*' / '/') :} |} Sp
+	eq		<- '=' Sp
+	opBrack	<- '(' Sp
+	clBrack <- ')' Sp
+	id 		<- {| {:tag: '' -> 'id':}  {:value: [a-zA-Z_][a-zA-Z0-9_]*:} |} Sp
+	num		<- {| {:tag: '' -> 'num':} {:value: [0-9][0-9]*('.'([0-9]^+1)^NumErr )^-1 :} |} Sp
+	Sp		<- (%s / %nl)*
 ]]..
-	"TermErr		<- ('' -> 'TermErr' => save_err) " .. rec('op1 / ")"') .. '\n' ..
-	"FactorErr		<- ('' -> 'FactorErr' => save_err) " .. rec('op1 / op2 / ")"') .. '\n' ..
-	"ExpErr			<- ('' -> 'ExpErr' => save_err) " .. rec('")"') .. '\n' ..
-	"ClBrackErr		<- ('' -> 'ClBrackErr' => save_err) " .. rec('op1 / op2 / ")"') .. '\n' ..
-	"NumErr			<- ('' -> 'NumErr' => save_err) " .. rec('op1 / op2 / ")"') .. '\n' ..
-	"AttErr			<- ('' -> 'AttErr' => save_err) " .. rec('exp / att') .. '\n'
+	"TermErr		<- ('' -> 'TermErr' => save_err) 	{| {:tag: '' -> 'term'  :} {:value: '' -> 'DEFAULT_NODE' :} |}" .. rec('op1 / clBrack') .. '\n' ..
+	"ExpErr			<- ('' -> 'ExpErr' => save_err) 	{| {:tag: '' -> 'exp' 	:} {:value: '' -> 'DEFAULT_NODE' :} |}" .. rec('")"') .. '\n' ..
+	"ClBrackErr		<- ('' -> 'ClBrackErr' => save_err) " .. rec('op1 / op2 / clBrack') .. '\n' ..
+	"NumErr			<- ('' -> 'NumErr' => save_err) 	{| {:tag: '' -> 'num' 	:} {:value: '' -> 'DEFAULT_NODE' :} |}" .. rec('op1 / op2 / clBrack') .. '\n' ..
+	"AttErr			<- ('' -> 'AttErr' => save_err) 	{| {:tag: '' -> 'att'	:} {:value: '' -> 'DEFAULT_NODE' :} |}" .. rec('id / num / opBrack') .. '\n' ..
+	"EndInput		<- ('' -> 'EndInput' => save_err) " .. rec('!.')
 , def)
+
+-- Obs.: Na label TermErr, captura um nó do tipo expretion, pois mudei a gramática
 
 local Parser = {}
 
 function Parser.parse(str)
 	errors = {}
 	local ast, err, pos = g:match(str)
-	local row, col = re.calcline(str,pos)
-	pos = {row=row, col=col}
-	return ast, err, pos
+	if pos then
+		local row, col = re.calcline(str,pos)
+		pos = {row=row, col=col}
+	end
+	return ast, err, pos or errors
+end
+
+function Parser.printAST(node, spaces)
+	if spaces == nil then
+		spaces = 0
+	end
+
+	for i=1, spaces do
+		io.write " "
+	end
+
+	if node.tag ~= nil then
+		io.write(node.tag..': ')
+		if type(node.value) == 'table' then
+			print ""
+			for k,v in pairs(node.value) do
+				Parser.printAST(v, spaces+2)
+			end
+		else
+			print('"'..node.value..'"')
+		end
+	else
+		print "Inesperado"
+	end
 end
 
 return Parser
--- test.lua
+++ test.lua
@@ -2,32 +2,48 @@ local parser = require 'parser'
 
 local str
 
-function myAssert(str,expec)
+function myAssert(str)
 	local ast, err, pos = parser.parse(str)
+	io.write 'Expressão:\n"'
+	io.write(str)
+	io.write '"\n'
 
-	if expec ~= ast then
+	if ast then
 		if ast then
-			io.write "\n\nResultado: "
-			print(ast)
-			io.write "Esperado: "
-			print(expec)
+			print '\n\n============================'
+			print "AST:"
+			print "----------------------------"
+			parser.printAST(ast)
+			print "============================"
 		else
 			print "Erro não capturado!"
 			io.write "Tipo: " print(err)
 			io.write "Posição: " print('(linha: '..pos.row..', coluna: '..pos.col..')')
 		end
 		io.write '\n'
-
+	else
 		error("Resultado inesperado!")
 	end
+
+	if err then
+		print("Falha no parser! (linha: "..pos.line..', coluna: '..pos.col..')')
+	else
+		print(#pos .. " erro(s) encontrado(s):")
+		for i, v in ipairs(pos) do
+			print("\t(linha: " ..v.line..', coluna: '..v.col..'): '..v.msg)
+		end
+	end
+
+	print ""
+
 end
 
 print "> Testando Labels..."
 
-io.write ">> Iniciando teste da label AttErr... "
+print ">> Iniciando teste da label AttErr..."
 
 str = "a = "
-myAssert(str, str:len()+1)	-- str:len()+1, significa que o parser analisou toda a entrada
+myAssert(str, #str+1)	-- str:len()+1, significa que o parser analisou toda a entrada
 
 str = [=[
 oi =
@@ -43,6 +59,12 @@ a+b*(25+30) a =
 b
 ]=]
 myAssert(str, str:len()+1)
+
+str = "a = , a = 2 + 2)"
+myAssert(str, #str+1)
+
+str = "2 * "
+myAssert(str, #str+1)
 
 io.write "OK\n"
 
